57ea95c4a4bb1c23e5e3c22017366e35c3e8471f
Marco Aurélio G. Da Silva <marcoc2@gmail.com>
2022-09-26 11:19:09 -0300

    Import changes

--- optimizedSD/optimized_img2img.py
+++ optimizedSD/optimized_img2img.py
@@ -14,7 +14,7 @@ from torch import autocast
 from contextlib import contextmanager, nullcontext
 from einops import rearrange, repeat
 from ldm.util import instantiate_from_config
-from optimUtils import split_weighted_subprompts, logger
+from optimizedSD.optimUtils import split_weighted_subprompts, logger
 from transformers import logging
 import pandas as pd
 logging.set_verbosity_error()
--- optimizedSD/optimized_txt2img.py
+++ optimizedSD/optimized_txt2img.py
@@ -13,7 +13,7 @@ from pytorch_lightning import seed_everything
 from torch import autocast
 from contextlib import contextmanager, nullcontext
 from ldm.util import instantiate_from_config
-from optimUtils import split_weighted_subprompts, logger
+from optimizedSD.optimUtils import split_weighted_subprompts, logger
 from transformers import logging
 # from samplers import CompVisDenoiser
 logging.set_verbosity_error()
